11b726c63b22671f622fb74f3c5bc9d1d3dfef93
Emanuele DelBono <emanuele@codiceplastico.com>
2017-12-28 09:11:22 +0100

    basic support for middleware

--- __tests__/withState.test.js
+++ __tests__/withState.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import TestComponent from './fake/TestComponent'
 import withState from '../lib/withState'
+import dispatcher from '../lib/dispatcher'
 
 test('withState should wrap', () => {
   const component = renderer.create(<TestComponent />)
@@ -23,4 +24,19 @@ test('withState supports partial application ', () => {
 
   tree = renderer.create(<BWithName />).toJSON()
   expect(tree).toMatchSnapshot()
+})
+
+test('withState should accept a pre-middleware function', () => {
+  const A = ({ name }) => <div>Hello, {name}!</div>
+  let done = false
+  const middleware = (state, action) => { done = true }
+
+  const Wrapped = withState([], {}, middleware)(A)
+  const component = renderer.create(<Wrapped />)
+  let tree = component.toJSON()
+
+  dispatcher.dispatch({type: 'FAKE_ACTION', content: {}})
+
+  expect(tree).toMatchSnapshot()
+  expect(done).toBeTruthy()
 })
